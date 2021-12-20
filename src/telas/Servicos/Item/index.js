b893da2a0405a75c13d4d5924867ac7d26dab0e2
Isabela Maria Da Campo <isabeladacampo@gmail.com>
2021-12-20 11:19:55 -0300

    Adicionado carrinho e valor do carrinho

--- src/telas/Servicos/Item/index.js
+++ src/telas/Servicos/Item/index.js
@@ -46,7 +46,7 @@ export default function Item({nome, preco, descricao}) {
             }</Text>
           </View>
         </View>
-        <Botao valor="Adicionar" acao={() => {}} />
+        <Botao valor="Adicionar ao carrinho" acao={() => {}} />
       </View>
     }
     <View style={estilos.divisor} />
